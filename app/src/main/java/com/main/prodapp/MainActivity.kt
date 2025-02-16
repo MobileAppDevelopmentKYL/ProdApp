db9493f06e702ca330d1f9915c53c92b7659055f
Bill Yang <joyang12345@gmail.com>
2025-02-16 14:51:40 -0500

    LogCat for LifeCycle

--- app/src/main/java/com/main/prodapp/MainActivity.kt
+++ app/src/main/java/com/main/prodapp/MainActivity.kt
@@ -1,32 +1,48 @@
 package com.main.prodapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 
+private const val TAG = "MainActivity"
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "Start onCreate")
     }
 
     override fun onStart() {
         super.onStart()
+
+        Log.d(TAG, "Start onStart")
     }
 
     override fun onResume() {
         super.onResume()
+
+        Log.d(TAG, "Start onResume")
     }
 
     override fun onPause() {
         super.onPause()
+
+        Log.d(TAG, "Start onPause")
     }
 
     override fun onStop() {
         super.onStop()
+
+        Log.d(TAG, "Start onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
+
+        Log.d(TAG, "Start onDestroy")
     }
 }
 
