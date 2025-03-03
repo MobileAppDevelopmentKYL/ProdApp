9719f71620355cdbeff1f1cf459a1bf44741f267
Msk142 <khjun7830@gmail.com>
2025-03-03 01:35:58 -0500

    view holder

--- app/src/main/java/com/main/prodapp/fragments/TodoListAdapter.kt
+++ app/src/main/java/com/main/prodapp/fragments/TodoListAdapter.kt
@@ -1,4 +1,10 @@
 package com.main.prodapp.fragments
 
-class TodoListAdapter {
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoListAdapter (private val todo: List<TodoData>) : RecyclerView.Adapter<TodoViewHolder>() {
+
+    override fun onCreateViewHolder{
+
+    }
 }
