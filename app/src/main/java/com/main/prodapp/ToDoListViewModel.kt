eade4598bef14925f785af2b35ab50f4fa3a2a05
SimonL9889 <113265728+MqLei423@users.noreply.github.com>
2025-03-04 23:50:04 -0500

    add some functions

--- app/src/main/java/com/main/prodapp/ToDoListViewModel.kt
+++ app/src/main/java/com/main/prodapp/ToDoListViewModel.kt
@@ -1,13 +1,33 @@
 package com.main.prodapp
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.main.prodapp.fragments.TodoData
 
 private const val TAG = "ToDoListViewModel"
 
 class ToDoListViewModel : ViewModel() {
+
+    private var toDoList:List<TodoData> = emptyList()
+
     init {
         Log.d(TAG, "ViewModel instance created")
+    }
+
+    fun addTodo(todo: TodoData) {
+        toDoList = toDoList + todo
+        Log.d(TAG, "item added: $todo")
+    }
+
+    fun removeTodo(todo: TodoData) {
+        toDoList = toDoList - todo
+        Log.d(TAG, "item removed: $todo")
+    }
+
+    fun getTodos(): List<TodoData> {
+        return toDoList
     }
 
     override fun onCleared() {
