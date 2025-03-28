60a73a892abc9fb98e27a39864cbb540e6441bf3
Msk142 <khjun7830@gmail.com>
2025-03-28 01:30:23 -0400

    change

---
+++ app/src/main/java/com/main/prodapp/fragments/CharacterFragment.kt
@@ -0,0 +1,75 @@
+package com.main.prodapp.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.main.prodapp.databinding.FragmentSettingBinding
+
+private const val TAG = "CharacterFragment"
+
+class CharacterFragment : Fragment() {
+    private var _binding : FragmentSettingBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "Start onCreate")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG, "Start onCreateView")
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d(TAG, "Start onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d(TAG, "Start onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d(TAG, "Start onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(TAG, "Start onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG, "Start onDestroy")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        Log.d(TAG, "Start onDestoryView")
+    }
+}
